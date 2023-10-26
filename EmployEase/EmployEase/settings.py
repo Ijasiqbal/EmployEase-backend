cdb159c6c60a5ebc856dad82b8e97c0dc3c4a12f
Ijas Iqbal <ijasiqbal4@gmail.com>
2023-10-26 22:25:59 +0530

    collect static

--- EmployEase/EmployEase/settings.py
+++ EmployEase/EmployEase/settings.py
@@ -29,6 +29,12 @@ DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_METHODS = [
+    'GET',
+    'POST',
+    'PUT',
+    'DELETE',
+]
 
 
 # Application definition
@@ -48,9 +54,9 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
