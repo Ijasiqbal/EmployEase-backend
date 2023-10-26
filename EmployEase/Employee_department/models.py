0c0873f30daafd575761355222dbc44bea39be99
Ijas Iqbal <ijasiqbal4@gmail.com>
2023-10-26 19:26:46 +0530

    configured for railway

--- EmployEase/Employee_department/models.py
+++ EmployEase/Employee_department/models.py
@@ -3,11 +3,12 @@ from django.db import models
 class Department(models.Model):
     name = models.CharField(max_length=100, null=True, blank=True)
     location = models.CharField(max_length=100, null=True, blank=True)
-    manager_id = models.PositiveIntegerField(null=True, blank=True)
+    managerId = models.PositiveIntegerField(null=True, blank=True)
 
 class Employee(models.Model):
-    name = models.CharField(max_length=100, null=True, blank=True)
+    name = models.CharField(max_length=100)
     email = models.EmailField(null=True, blank=True)
-    contact_number = models.CharField(max_length=15, null=True, blank=True)
-    date_of_joining = models.DateField(null=True, blank=True)
-    years_of_experience = models.PositiveSmallIntegerField(null=True, blank=True)
+    contactNumber = models.CharField(max_length=15, null=True, blank=True)
+    dateOfJoining = models.DateField(null=True, blank=True)
+    yearsOfExperience = models.PositiveSmallIntegerField(null=True, blank=True)
+    department = models.ForeignKey(Department, on_delete=models.PROTECT)
